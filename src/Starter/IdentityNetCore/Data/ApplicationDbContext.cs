6dfb107f516b7e21a4e7e52d208d697a8dede660
Jesper <zacban@gmail.com>
2026-03-16 14:17:05 +0100

    Refactor code structure for improved readability and maintainability

---
+++ src/Starter/IdentityNetCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityNetCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext() { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    }
+}
